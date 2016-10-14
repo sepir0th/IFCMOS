e05669d7ba68158b49adb1e08510bf9fe789c6ab
Erwin Lim <erwin.lim@infoconnect.com.my>
2016-10-14 17:42:54 +0700

    Various bug Fixing on Illustration Module

--- MobileOfficeSolution/MobileOfficeSolution/RelationshipPopoverViewController.m
+++ MobileOfficeSolution/MobileOfficeSolution/RelationshipPopoverViewController.m
@@ -50,7 +50,7 @@
     NSArray *dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *docsDir = [dirPaths objectAtIndex:0];
     
-    databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent: @"hladb.sqlite"]];
+    databasePath = [[NSString alloc] initWithString: [docsDir stringByAppendingPathComponent: @"MOSDB.sqlite"]];
     
     
     
